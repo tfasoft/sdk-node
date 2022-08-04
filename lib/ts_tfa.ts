93d37ddeab29f3dcd851bbb7c50e09c94ca10f4f
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-04 21:51:26 +0430

    TypeScript updated

--- lib/ts_tfa.ts
+++ lib/ts_tfa.ts
@@ -2,13 +2,20 @@ import axios from 'axios';
 
 class TFA {
     access_token: string;
+    baseUrl: string;
 
     constructor(access_token) {
         this.access_token = access_token;
+        this.baseUrl = "https://tele-fa-api.herokuapp.com";
     }
 
-    authUser(user_id: string) {
-        return axios.get(`https://tele-fa-api.herokuapp.com/api/access/${this.access_token}/${user_id}`)
+    authUser(user_token: string) {
+        const data = {
+            "access_token": this.access_token,
+            "user_token": user_token,
+        }
+
+        return axios.post(`${this.baseUrl}/api/auth/access`, data)
             .then((response) => {
                 const data = {
                     status: response.status,
